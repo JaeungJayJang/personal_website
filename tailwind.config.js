076004f88832b4622bab53f1d7d1373b9d956ab9
wodndckato <wodndckato@gmail.com>
2023-08-23 02:00:34 -0400

    updated color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,43 @@ module.exports = {
 	theme: {
 		extend: {
 			colors: {
-				'primary': "#394460",
-				"secondary": "#EF8354",
-				"tertiary": "#2D3142"
+				'primary': {
+					DEFAULT: "#2e3f5b",
+					90: "#43526b",
+					80: "#58657c",
+					70: "#6d798c",
+					60: "#828c9d",
+					50: "#979fad",
+					40: "#abb2bd",
+					30: "#c0c5ce",
+					20: "#d5d9de",
+					10: "#eaecef",
+
+				},
+				"secondary": {
+					DEFAULT: "#ef8354",
+					90: "#f18f65",
+					80: "#f29c76",
+					70: "#f4a887",
+					60: "#f5b598",
+					50: "#f7c1aa",
+					40: "#f9cdbb",
+					30: "#fadacc",
+					20: "#fce6dd",
+					10: "#fdf3ee",
+				},
+				"tertiary": {
+					DEFAULT: "#2d3142",
+					90: "#424655",
+					80: "#575a68",
+					70: "#6c6f7b",
+					60: "#81838e",
+					50: "#9698a1",
+					40: "#abadb3",
+					30: "#c0c1c6",
+					20: "#d5d6d9",
+					10: "#eaeaec",
+				}
 			},
 			listStyleType: {
 				line: "'--'",
