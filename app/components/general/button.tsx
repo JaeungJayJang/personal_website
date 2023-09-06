edd60963deec3aa9d8068e98c2448476a3bbf10b
qmffkem <wodndckato@gmail.com>
2023-09-06 00:39:29 -0400

    updated resume button to linkedin button  and enhance original button component

--- app/components/general/button.tsx
+++ app/components/general/button.tsx
@@ -1,19 +1,37 @@
 import React from "react";
 import Link from "next/link";
 
+
 interface props {
-    text : string;
+    text: string;
     url?: string;
+    upper?: boolean;
+    color?: string;
 }
 
-const MainButton = ({text, url}: props) => {
-    return (
-        <button className = "px-8 py-4 bg-tertiary ">
-            <p className = "text-white font-bold text-base">
-                {text.toUpperCase()}
-            </p>
-        </button>
-    )
+const MainButton = ({ text, url, upper = true }: props) => {
+    if (url) {
+        return (
+            <Link
+                href={url}
+            >
+                <button className="group px-8 py-4 bg-tertiary">
+                    <p className="text-white font-bold text-base group-hover:text-secondary">
+                        {upper ? text.toUpperCase() : text}
+                    </p>
+                </button>
+            </Link>
+        )
+    }
+    else {
+        return (
+            <button className="px-8 py-4 bg-tertiary ">
+                <p className="text-white font-bold text-base">
+                    {upper ? text.toUpperCase() : text}
+                </p>
+            </button>
+        )
+    }
 }
 
 export default MainButton;
