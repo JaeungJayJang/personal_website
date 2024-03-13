58132ccc6973fb0532d115d55f7472a970dd634e
qmffkem <wodndckato@gmail.com>
2024-03-13 15:52:07 -0400

    feat: extend button to React button for events

--- app/components/general/button.tsx
+++ app/components/general/button.tsx
@@ -1,6 +1,7 @@
+'use client'
+
 import React from "react";
 import Link from "next/link";
-
 
 interface props {
     text: string;
@@ -9,13 +10,16 @@ interface props {
     color?: string;
 }
 
-const MainButton = ({ text, url, upper = true }: props) => {
+type MainButtonProps = props & React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+
+const MainButton: React.FC<MainButtonProps>= ({ text, url, upper = true, color, children, ...props }) => {
     if (url) {
         return (
             <Link
                 href={url}
             >
-                <button className="group px-8 py-4 bg-tertiary">
+                <button className={`group px-8 py-4 bg-tertiary ${color}`} {...props}>
                     <p className="text-white font-bold text-base group-hover:text-secondary">
                         {upper ? text.toUpperCase() : text}
                     </p>
@@ -25,7 +29,7 @@ const MainButton = ({ text, url, upper = true }: props) => {
     }
     else {
         return (
-            <button className="px-8 py-4 bg-tertiary ">
+            <button className={`px-8 py-4 bg-tertiary ${color}`} {...props}>
                 <p className="text-white font-bold text-base">
                     {upper ? text.toUpperCase() : text}
                 </p>
