4b7960529c365b51a07ba990599c898f2e2c2366
qmffkem <wodndckato@gmail.com>
2024-12-31 02:02:03 -0500

    feat: add scroll-based visibility toggle for mouse scroll icon in HeroContainer

--- app/components/container/HeroContainer.tsx
+++ app/components/container/HeroContainer.tsx
@@ -1,6 +1,25 @@
+'use client'
+
+import { useEffect, useState } from 'react';
+import { PiMouseScrollLight } from "react-icons/pi";
 import Spline from '@splinetool/react-spline';
 
 const HeroContainer = () => {
+    const [showIcon, setShowIcon] = useState(true);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 0) {
+                setShowIcon(false);
+            } else {
+                setShowIcon(true);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
     return (
         <div className="h-[calc(100vh+1000px)]">
             <div className="sticky top-0">
@@ -9,6 +28,9 @@ const HeroContainer = () => {
                         scene="https://prod.spline.design/0CFsJKSH-WJ2TaVR/scene.splinecode"
                     />
                 </div>
+            </div>
+            <div className={`absolute bottom-10 left-1/2 transform -translate-x-1/2 transition-opacity duration-300 ${showIcon ? 'opacity-100' : 'opacity-0'}`}>
+                <PiMouseScrollLight className="animate-bounce text-5xl " />
             </div>
         </div>
     );
