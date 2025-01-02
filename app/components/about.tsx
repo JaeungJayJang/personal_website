b5e74c3c8a6d49fc26adcd1b8635830f65103d4e
qmffkem <wodndckato@gmail.com>
2025-01-02 05:31:45 -0500

    feat: implement dynamic data loading for About component from JSON file and changed style

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -1,28 +1,66 @@
 import React from "react";
 import { FaLinkedinIn, FaGithub } from "react-icons/fa";
+import fs from "fs";
 
-import Hero from "./general/hero";
-import Container from "./general/container";
 
-const GITHUB_LINK = "https://github.com/qmffkem";
+const GITHUB_LINK = "https://github.com/jaeungjayjang";
 const LINKED_LINK = "https://www.linkedin.com/in/jaeung-jang/";
 
 
-const BlueSphereBio = () => {
+interface dataProps {
+    name: string;
+    firstName: string;
+    lastName: string;
+    alternativeName: string;
+    description: Array<string>;
+}
 
-    return (
-        <a className="inline-block text-red-400" href="https://bluespherebio.com/" target="_black">
-            BlueSphere Bio
-        </a>
-    )
+const getData = (): dataProps => {
+    const file = fs.readFileSync("data/about.json", { encoding: "ascii" });
+    let fileObj = JSON.parse(file);
+    return fileObj;
 }
 
 const About = () => {
-
+    const data = getData();
     return (
-        <Container id="about" height="h-screen">
-            <Hero/>
-        </Container>
+        // <Container id="about" height="h-screen">
+        //     <Hero/>
+        // </Container>
+        <div id="about" className="h-screen bg-background">
+            <div className="flex flex-col justify-center items-center gap-4 p-4 h-full bg-background">
+                <div className="flex flex-row justify-center h-full py-20 max-w-screen-xl text-white w-full">
+                    <div id="left" className="w-1/2">
+                    </div>
+                    <div id="right" className="w-1/2 flex flex-col justify-center items-center gap-4">
+
+                        <div className="w-full flex flex-col gap-4">
+                            <div className="text-5xl font-semibold">
+                                {`Hello, I'm ${data.name}`}
+                            </div>
+                            {
+                                data.description.map((desc: string, index: number) => {
+                                    return (
+                                        <div key={index} className="text">
+                                            {desc}
+                                        </div>
+                                    )
+                                })
+                            }
+
+                            <div className="flex gap-4 pt-12">
+                                <a href={GITHUB_LINK} target="_blank" rel="noreferrer">
+                                    <FaGithub className="text-2xl opacity-60 hover:opacity-100" />
+                                </a>
+                                <a href={LINKED_LINK} target="_blank" rel="noreferrer">
+                                    <FaLinkedinIn className="text-2xl opacity-60 hover:opacity-100" />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
