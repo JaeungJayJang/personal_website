d2d3b2de942639fda5c0fd115a275b106a464575
qmffkem <wodndckato@gmail.com>
2025-01-12 13:17:49 -0500

    refactor: simplify layout structure by removing unnecessary divs and comments

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,15 +18,12 @@ export default function RootLayout({
 	children: React.ReactNode
 }) {
 	return (
-		<html lang="en">
-			<body className={`${inter.className}`} >
+		<html lang="en" className="">
+			<body className={` ${inter.className}`}>
 				<Header />
-				<div className="flex flex-col overflow-hidden">
-					<div className="">
-						{children}
-					</div>
-					<div className="absolute w-[300px] h-[calc(100%-(100vh-60px))] bg-secondary z-[-1] top-[calc(100vh-60px)] left-[calc(50%-150px+400px)]"></div>
-				</div>
+				{children}
+				{/* <div className="flex flex-col overflow-hidden">
+				</div> */}
 				{/* <Footer /> */}
 			</body>
 		</html>
