d19a3dfb68f10b0127e3b09b2ae320cc9c1f3c60
qmffkem <wodndckato@gmail.com>
2025-01-12 13:18:06 -0500

    feat: enhance Experiences component layout and add long description support

--- app/components/experiences.tsx
+++ app/components/experiences.tsx
@@ -10,6 +10,7 @@ interface dataProps {
     start_date: Date;
     end_date: Date | undefined;
     descriptions: Array<string>;
+    description_long: String;
     skills: Array<string>;
 }
 
@@ -27,35 +28,42 @@ const Experiences = () => {
     return (
         // <div className="relative flex p-4 justify-center bg-primary z-0">
         //     <div className="flex flex-col justify-center h-full py-16 max-w-screen-lg text-white">
-        <Container id="experiences" color="primary">
-            <div className="flex flex-col justify-center items-left gap-4">
-                <h2 className="text-4xl font-bold">Companies I have worked with</h2>
-                <div className="flex flex-col gap-4 drop-shadow-xl">
-                    {data &&
-                        data.map((experneice, i) => {
-                            const startDate = new Date(experneice.start_date)
-                            const endDate = experneice.end_date ? new Date(experneice.end_date).getFullYear() : "present".toUpperCase();
-                            return (
-                                <div key={`experience_${i}`} className="flex flex-col py-4 gap-7">
-                                    <p className="pl-4">
-                                        {startDate.getFullYear()} - {endDate}
-                                    </p>
-                                    <CompanyCard
-                                        role={experneice.role}
-                                        company_name={experneice.company_name}
-                                        company_link={experneice.company_link}
-                                        start_date={experneice.start_date}
-                                        end_date={experneice.end_date}
-                                        descriptions={experneice.descriptions}
-                                        skills={experneice.skills}
-                                    />
-                                </div>
-                            )
-                        })
-                    }
+        <div id="about" className="h-full bg-background">
+            <div className="flex flex-col justify-center items-center gap-4 p-4 bg-background">
+                <div className="flex flex-row justify-center h-full pb-20 max-w-screen-xl text-white w-full">
+                    <div className="w-full flex flex-col gap-4">
+                        {data &&
+                            data.map((experneice, i) => {
+                                const startDate = new Date(experneice.start_date)
+                                const endDate = experneice.end_date ? new Date(experneice.end_date).getFullYear() : "present".toUpperCase();
+
+                                return (
+                                    <div key={`experience_${i}`} className="flex flex-col lg:flex-row w-full">
+                                        <div className="py-4 min-w-[15%]">
+                                            <p className="">
+                                                {startDate.getFullYear()} - {endDate}
+                                            </p>
+                                        </div>
+                                        <div className="w-full">
+                                            <CompanyCard
+                                                role={experneice.role}
+                                                company_name={experneice.company_name}
+                                                company_link={experneice.company_link}
+                                                start_date={experneice.start_date}
+                                                end_date={experneice.end_date}
+                                                descriptions={experneice.descriptions}
+                                                description_long={experneice.description_long}
+                                                skills={experneice.skills}
+                                            />
+                                        </div>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
                 </div>
             </div>
-        </Container>
+        </div>
     )
 }
 
