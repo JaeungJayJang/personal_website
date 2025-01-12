0d8d952a927eeedd512b3c1bf7e5f66953e9e878
qmffkem <wodndckato@gmail.com>
2025-01-12 13:40:15 -0500

    refactor: simplify Projects component layout and improve styling

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 import ProjectCard from "./general/project";
-import Container from "./general/container";
 import LoadMoreButton from "./general/load_more_button";
 
 interface dataProps {
@@ -30,39 +29,38 @@ const Projects = () => {
     let data = getData();
 
     return (
-        <Container id="projects" >
-            <div className="flex flex-col justify-center items-left gap-4">
-                <h2 className="text-4xl font-bold text-black">
-                    Side Projects
-                </h2>
-                <div className="flex flex-col gap-4 drop-shadow-xl lg:flex-row lg:grid lg:grid-cols-3">
-                    {data && data.length > 0 ?
-                        data.map((project, i) => {
-                            return (
-                                <div key={`project_${i}`} className="flex flex-col gap-7 lg:flex lg:aspect-square flex-grow">
+        <div id="projects" className="h-full bg-background">
+            <div className="flex flex-col justify-center items-center gap-4 p-4 bg-background">
+                <div className="flex flex-row justify-center h-full pb-20 max-w-screen-xl text-white w-full">
+                    <div className="w-full flex flex-col gap-4">
+                        <h2 className="text-3xl font-semibold">
+                            Side Projects
+                        </h2>
+                        <div className="flex flex-col gap-4 drop-shadow-xl lg:flex-row md:grid md:grid-cols-3 lg:grid-cols-4">
+                            {data && data.length > 0 ?
+                                data.map((project, i) => {
+                                    return (
+                                        <div key={`project_${i}`} className="flex flex-col gap-7 lg:flex lg:aspect-square flex-grow">
 
-                                    <ProjectCard
-                                        name={project.name}
-                                        description={project.description}
-                                        skills={project.skills}
-                                        link={project.link}
-                                    />
+                                            <ProjectCard
+                                                name={project.name}
+                                                description={project.description}
+                                                skills={project.skills}
+                                                link={project.link}
+                                            />
+                                        </div>
+                                    )
+                                })
+                                :
+                                <div className="text-black">
+                                    More to come..
                                 </div>
-                            )
-                        })
-                        :
-                        <div className="text-black">
-                            More to come..
+                            }
                         </div>
-                    }
+                    </div>
                 </div>
-
-                {/* {
-                    data && (data.length > LIMIT) &&
-                    <LoadMoreButton />
-                } */}
             </div>
-        </Container>
+        </div>
     )
 }
 
