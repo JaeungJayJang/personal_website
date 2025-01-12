2d09e0260c231b0b8cc4d85d2a84590f42789c12
qmffkem <wodndckato@gmail.com>
2025-01-12 14:21:16 -0500

    refactor: integrate HeroContainer component and clean up commented code

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,32 @@
 import React from "react";
 import About from "./components/about";
+import AboutT from "./components/about_GMG_style";
 import Experiences from "./components/experiences";
 import Projects from "./components/projects";
 import Contact from "./components/contact";
+import HeroContainer from "@/app/components/container/HeroContainer";
+import Spline from '@splinetool/react-spline';
 
 const HomePage = () => {
     return (
         <>
+            <HeroContainer />
             <About />
+            {/* <div className="h-40 bg-green-100 relative">
+                test
+            </div> */}
+            {/* <div className="h-40 bg-green-100 relative">
+                test
+            </div> */}
+            {/* <div className="h-40 bg-green-100 relative">
+                test
+            </div> */}
             <Experiences />
             <Projects />
-            <Contact/>
+            {/* <div className="h-[3000px] bg-background">
+
+            </div> */}
+            {/* <Contact /> */}
         </>
     )
 }
