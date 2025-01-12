536a19a1d8702329c38124a5fdcd7332fa13c445
qmffkem <wodndckato@gmail.com>
2025-01-12 14:20:14 -0500

    refactor: restructure footer component layout and enhance styling

--- app/components/layout/footer.tsx
+++ app/components/layout/footer.tsx
@@ -2,9 +2,13 @@ import Container from "../general/container";
 
 const Footer = () => {
     return (
-        <Container color="tertiary">
-            <p>Designed by Jaeung Jang</p>
-        </Container>
+        <div id="experiences" className="h-full bg-background">
+            <div className="flex flex-col justify-center items-center gap-4 p-4 bg-background">
+                <div className="flex flex-row justify-center h-full py-8 max-w-screen-xl text-white w-full border-t-[1px] border-opacity-5 ">
+                    <p>Designed by Jaeung Jang</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
